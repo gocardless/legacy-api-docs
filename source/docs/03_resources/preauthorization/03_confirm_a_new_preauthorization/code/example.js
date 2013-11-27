8311b40ecd51969fe246f2f87a4c1313c9049821
Andrew Appleton <andrew@floatleft.com>
2013-11-27 09:17:35 +0000

    Update node docs for simplified confirm step

--- source/docs/03_resources/preauthorization/03_confirm_a_new_preauthorization/code/example.js
+++ source/docs/03_resources/preauthorization/03_confirm_a_new_preauthorization/code/example.js
@@ -1,13 +1,7 @@
 // Assuming an Express app
 app.get('/gocardless_callback', function(req, res) {
-  var verified = gocardless.verifySignature(req.query, myAppSecret);
-  if (!verified) return res.end(401);
-
-  gocardless.confirmResource({
-    resource_id: req.query.resource_id,
-    resource_type: req.query.resource_type
-  }, function(err, request, body) {
-    if (err) res.end('error confirming bill');
+  gocardless.confirmResource(req.params, function(err, request, body) {
+    if (err) return res.end(401, err);
     res.render('success');
   });
 });
--- source/docs/03_resources/subscription/03_confirm_a_new_subscription/code/example.js
+++ source/docs/03_resources/subscription/03_confirm_a_new_subscription/code/example.js
@@ -1,13 +1,7 @@
+// Assuming an Express app
 app.get('/gocardless_callback', function(req, res) {
-  if (!gocardless.verifySignature(req.params, myAppSecret)) return res.end(403);
-
-  gocardless.confirmResource({
-    resource_id: req.params.resource_id,
-    resource_type: req.params.resource_type
-  }, function(err, request, body) {
-    // Handle error or retry
-    JSON.parse(body);
+  gocardless.confirmResource(req.params, function(err, request, body) {
+    if (err) return res.end(401, err);
+    res.render('success');
   });
-
-  res.end(200);
 });
