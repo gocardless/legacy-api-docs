06aa8f22acf6465778a58f7f5ddff4fa564dbbe6
Andrew Appleton <andrew@floatleft.com>
2014-01-06 11:16:22 +0000

    Update confirm subscription and preauth example

--- source/docs/03_resources/subscription/03_confirm_a_new_subscription/code/example.js
+++ source/docs/03_resources/subscription/03_confirm_a_new_subscription/code/example.js
@@ -1,6 +1,7 @@
 // Assuming an Express app
 app.get('/gocardless_callback', function(req, res) {
-  gocardless.confirmResource(req.params, function(err, request, body) {
+  // Check the signature and POST back to GoCardless
+  gocardless.confirmResource(req.query, function(err, request, body) {
     if (err) return res.end(401, err);
     res.render('success');
   });
