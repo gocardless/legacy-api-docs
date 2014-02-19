2f54588b1752faa354c18ebfd0806babd51128a5
bamesjerry <jamesjohnberry@gmail.com>
2014-02-19 16:27:27 +0000

    Fixed typo that called wrong file

--- source/docs/03_resources/preauthorization/07_cancel_a_pre-authorization/code/example.php
+++ source/docs/03_resources/preauthorization/07_cancel_a_pre-authorization/code/example.php
@@ -2,6 +2,6 @@
 /*
 Example request
 */
-$pre_auth = GoCardless_Preauthorization::find('#PREAUTH ID#')->cancel()
+$pre_auth = GoCardless_PreAuthorization::find('#PREAUTH ID#')->cancel()
 
 print_($pre_atuh);
