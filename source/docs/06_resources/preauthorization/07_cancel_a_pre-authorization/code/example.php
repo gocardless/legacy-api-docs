2e5247d794464ac8320a6e17d215cfec441f4f60
JacobPgn <jacob@gocardless.com>
2014-02-21 21:09:09 +0000

    Fixed typos

--- source/docs/06_resources/preauthorization/07_cancel_a_pre-authorization/code/example.php
+++ source/docs/06_resources/preauthorization/07_cancel_a_pre-authorization/code/example.php
@@ -2,6 +2,6 @@
 /*
 Example request
 */
-$pre_auth = GoCardless_PreAuthorization::find('#PREAUTH ID#')->cancel()
+$pre_auth = GoCardless_PreAuthorization::find('#PREAUTH ID#')->cancel();
 
-print_($pre_atuh);
+print_r($pre_auth);
