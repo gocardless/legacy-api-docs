1ffc28c240e74961d94df8b7f6ef66366efe0b17
JacobPgn <jacob@gocardless.com>
2014-02-21 14:55:48 +0000

    Fixed typo

--- source/docs/03_resources/subscription/05_cancel_a_subscription/code/example.php
+++ source/docs/03_resources/subscription/05_cancel_a_subscription/code/example.php
@@ -2,6 +2,6 @@
 /*
 Example request
 */
-$subscription = GoCardless_Subscription::find('#SUBSCRIPTION ID#')->cancel()
+$subscription = GoCardless_Subscription::find('#SUBSCRIPTION ID#')->cancel();
 
 print_r($subscription);
