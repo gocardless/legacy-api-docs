f8a8867d08284fdc3eb798b6906e75a53c455d73
Alasdair Monk <alasdair.monk@gmail.com>
2014-10-21 15:49:48 +0100

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var markdown = require('gulp-markdown');
 var concat = require('gulp-concat');
 var clean = require('gulp-clean');
 var connect = require('gulp-connect');
-var sass = require('gulp-sass');
+var sass = require('gulp-ruby-sass');
 var open = require("gulp-open");
 var swig = require("gulp-swig");
 var headerfooter = require('gulp-headerfooter');
@@ -11,13 +11,11 @@ var headerfooter = require('gulp-headerfooter');
 gulp.task('default', ['docs']);
 
 gulp.task('docs', function () {
-    return gulp.src('source/docs/**/*.md')
-        .pipe(markdown())
-        .pipe(concat('index.html'))
-        .pipe(headerfooter.header('./source/layouts/header.html'))
-        .pipe(headerfooter.footer('./source/layouts/footer.html'))
-        .pipe(gulp.dest('./_site/'))
-        .pipe(connect.reload());
+  return gulp.src('source/docs/**/*.md')
+    .pipe(markdown())
+    .pipe(concat('index.html'))
+    .pipe(gulp.dest('./_site/'))
+    .pipe(connect.reload());
 });
 
 gulp.task('images', function () {
@@ -28,12 +26,9 @@ gulp.task('images', function () {
 
 gulp.task('sass', function () {
   gulp.src('./source/stylesheets/**/*.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .on('error', function(err){
-        displayError(err);
-    })
+    .pipe(sass())
+    .pipe(concat('all.css'))
+    .on('error', function (err) { console.log(err.message); })
     .pipe(gulp.dest('_site/stylesheets/'));
 });
 
