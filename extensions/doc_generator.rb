fb9523a8d3e479e347c2e9b70592a4620aefca99
Alasdair Monk <alasdair.monk@gmail.com>
2014-10-24 16:38:32 +0100

    Remove Middleman/Ruby related files

--- extensions/doc_generator.rb
+++
@@ -1,8 +0,0 @@
-require "middleman-core"
-
-require_relative "doc_generator/version"
-require_relative "doc_generator/extension"
-
-::Middleman::Extensions.register(:doc_generator) do
-  ::Middleman::DocGeneratorExtension
-end
--- extensions/doc_generator/version.rb
+++
@@ -1,5 +0,0 @@
-module Middleman
-  module DocGenerator
-    VERSION = "0.0.2"
-  end
-end
